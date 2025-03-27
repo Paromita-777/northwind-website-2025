9e3fc736435d192b8624f0faf368afd922ac9f82
Paromita Sarkar <paromita.sarkar.srk@gmail.com>
2025-03-27 19:44:22 +1100

    Added index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+  // Config
+  $title = "Home";
+
+  // Start output buffering (trap output - don't display it yet)
+  ob_start();
+
+  // Include the page-specific template
+  include_once "templates/_indexPage.html.php";
+
+  // Stop output buffering - store output into the $content variable
+  $contentzzz = ob_get_clean();
+
+  // Include the main layout template
+  include_once "templates/_layout.html.php";
