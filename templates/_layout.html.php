f3aa5722c4b0fff8a3f0fb17987d4e1d27573f4b
Paromita Sarkar <paromita.sarkar.srk@gmail.com>
2025-03-27 19:46:58 +1100

    added templates for layout and index

---
+++ templates/_layout.html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $title ?? "NO TITLE" ?> - Northwind</title>
+</head>
+<body>
+  
+  <?= $content ?? 'NO CONTENT - $content not defined' ?>
+
+</body>
+</html>
